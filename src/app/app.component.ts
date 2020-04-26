29169e9f533a4526efe9fb48b82aa82375dbd1be
Sebastian Giro <giro.sebastian@gmail.com>
2020-04-26 11:32:54 -0300

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sebastian-app';
+
+  toggleFullScreen() {
+    const isFullscreen = document['fullscreen'] || document['webkitIsFullScreen'] || document['mozFullScreen'] || false;
+
+    if (isFullscreen) {
+      this.closeFullscreen();
+    } else {
+      this.openFullscreen();
+    }
+  }
+
+  openFullscreen() {
+    const elem = document.documentElement;
+    const methodToBeInvoked = elem.requestFullscreen ||
+                              elem['webkitRequestFullScreen'] ||
+                              elem['mozRequestFullscreen'] ||
+                              elem['msRequestFullscreen'];
+    if (methodToBeInvoked) { methodToBeInvoked.call(elem); }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    const elem = document;
+    const methodToBeInvoked = elem.exitFullscreen ||
+      elem['webkitExitFullscreen'] ||
+      elem['mozCancelFullScreen'] ||
+      elem['msExitFullscreen'];
+    if (methodToBeInvoked) { methodToBeInvoked.call(elem); }
+  }
 }
